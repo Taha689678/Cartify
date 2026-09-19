f7fa68ab9993c8d44690684b98ab41a59b955e5b
Malik Taha <maliktaha983@gmail.com>
2026-09-19 23:11:35 +0500

    .

--- client/src/api/paymentApi.js
+++ client/src/api/paymentApi.js
@@ -4,3 +4,11 @@ export const paymentApi = {
   initiatePayFastPayment: (data) => api.post("/payments/payfast/initiate", data),
   getPaymentByOrderId: (orderId) => api.get(`/payments/order/${orderId}`),
 };
+
+
+
+
+
+
+
+
